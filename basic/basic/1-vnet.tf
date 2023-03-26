e43751315e77e1ef74553aa32b767f6e7d1aeaf0
gowtham <medagowtham2@yahoo.com>
2023-03-26 14:14:48 +0530

    Added files for vm,vm_lb,vm_lb_count

---
+++ basic/basic/1-vnet.tf
@@ -0,0 +1,122 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "random_pet" "randy1" {
+  length = 1
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "terraform-farm-${random_pet.randy1.id}"
+  location = "East US"
+}
+
+
+resource "azurerm_virtual_network" "example" {
+  name                = "mgm-net"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.1.0.0/16"]
+
+  tags = {
+    environment = "Test"
+  }
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "subs"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "MySecurityGroup"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+locals {
+  sg-port = {
+    100 : 22,
+    110 = 80
+  }
+
+}
+
+resource "azurerm_network_security_rule" "example" {
+  for_each                    = local.sg-port
+  name                        = "sg-rule-${each.value}"
+  priority                    = each.key
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = each.value
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.example.name
+  network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+  depends_on = [
+    azurerm_network_security_rule.example
+  ]
+}
+
+#StandAlonePipWithNic
+/*
+resource "azurerm_public_ip" "example" {
+  name                = "myPips"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_network_interface" "example" {
+  # count               = 2
+  # name                = "meda-nic-${count.index}"
+  name                = "meda-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+*/
+
+#NicToAssociateWithSimpleLB
+resource "azurerm_network_interface" "example" {
+  name                = "meda-nic-SimpleLB"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+
+  ip_configuration {
+    name                          = "FrontPipforLB"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
---
+++ vm_with_lb_count/variable.tf
@@ -0,0 +1,3 @@
+variable "no_of_instance" {
+  default = "3"
+}
